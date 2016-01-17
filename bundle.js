755c84ff85494e37e82a20303c91f6617924458a
dschalk <schalkbox@gmail.com>
2016-01-16 19:31:58 -0500

    Changed a URL

--- bundle.js
+++ bundle.js
@@ -370,7 +370,7 @@
 	          _react2['default'].createElement('br', null),
 	          _react2['default'].createElement(
 	            'a',
-	            { href: 'http://schalk.net:3095' },
+	            { href: 'http://schalk.net:3000' },
 	            _react2['default'].createElement(
 	              'button',
 	              { style: _this.bool9 ? _this.style1 : _this.style2,
