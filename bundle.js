9e4cc4a28def2f5b63625276a6a77b36e5aa4ddf
dschalk <schalkbox@gmail.com>
2016-01-17 11:31:20 -0500

    Ran webpack again.

--- bundle.js
+++ bundle.js
@@ -353,7 +353,7 @@
 	          _react2['default'].createElement('br', null),
 	          _react2['default'].createElement(
 	            'a',
-	            { href: 'http://schalk.net:4001' },
+	            { href: 'http://schalk.net:3000' },
 	            _react2['default'].createElement(
 	              'button',
 	              { style: _this.bool6 ? _this.style1 : _this.style2,
